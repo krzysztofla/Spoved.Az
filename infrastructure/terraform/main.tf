e4abaf85b46d398cdd66b226238ea4c94e2f088f
krzysztofla <krzysztof.lach@icloud.com>
2023-12-12 11:43:09 +0100

    another try meh3

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -15,6 +15,10 @@ terraform {
     }
   }
   backend "azurerm" {
+    client_id            = var.client_id
+    client_secret        = var.client_secret
+    tenant_id            = var.tenant_id
+    subscription_id      = var.subscription_id
     resource_group_name  = "cloud-labs-dev"
     storage_account_name = "tfstatestorage0"
     container_name       = "tfstate"
