a78657fb558aa9466edf63f3dbfc4fc53c693a53
krzysztofla <krzysztof.lach@icloud.com>
2023-12-12 11:37:54 +0100

    another try meh

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,3 +1,12 @@
+variable "client_id" {
+}
+variable "client_secret" {
+}
+variable "tenant_id" {
+}
+variable "subscription_id" {
+}
+
 terraform {
   required_providers {
     azurerm = {
@@ -15,4 +24,8 @@ terraform {
 
 provider "azurerm" {
   features {}
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
