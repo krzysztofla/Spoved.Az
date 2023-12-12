a5a7a3d03f93f5575f3fa36c87ecf423b3d26de2
krzysztofla <krzysztof.lach@icloud.com>
2023-12-12 11:01:24 +0100

    Another try

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -6,20 +6,11 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "resource_group_name"
-    storage_account_name = "storage_account_name"
-    container_name       = "container_name"
-    key                  = "prod.terraform.tfstate"
+    resource_group_name  = "cloud-labs-dev"
+    storage_account_name = "tfstatestorage0"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
   }
-}
-
-provider "azurerm" {
-  features {
-  }
-  client_id       = "client_id"
-  client_secret   = "client_secret"
-  tenant_id       = "tenant_id"
-  subscription_id = "subscription_id"
 }
 
 # Configure the Microsoft Azure Provider
