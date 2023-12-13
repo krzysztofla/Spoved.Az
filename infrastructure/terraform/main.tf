49271c6d9112f64fb468c2488bbfd62a4e82a7d1
krzysztofla <krzysztof.lach@icloud.com>
2023-12-13 16:31:39 +0100

    Changing structure of tf file

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -16,3 +16,41 @@ terraform {
     key                  = "dev.terraform.tfstate"
   }
 }
+
+
+#####-------------------------------Infra configuration-------------------------------#####
+
+variable "aks_client_id" {
+  type = string
+}
+variable "aks_client_secret" {
+  type = string
+}
+
+resource "azurerm_resource_group" "aks" {
+  name     = "cloud-labs-dev"
+  location = "polandcentral"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "spoved-az-cluster"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  dns_prefix          = "spoved-dev"
+  kubernetes_version  = "1.28.2"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  service_principal {
+    client_id     = var.aks_client_id
+    client_secret = var.aks_client_secret
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+}
