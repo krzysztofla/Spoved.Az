54dd91157c8aebdf2aee0e4b6ae9144e4018772c
krzysztofla <krzysztof.lach@icloud.com>
2023-12-18 15:44:48 +0100

    Dns grou name for zone

--- infrastructure/dns/main.tf
+++ infrastructure/dns/main.tf
@@ -18,6 +18,6 @@ terraform {
 }
 
 resource "azurerm_dns_zone" "example" {
-  name                = "spoved.app.dev.pl"  
+  name                = "spoved.app"  
   resource_group_name = "cloud-labs-dev"
 }
